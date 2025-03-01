a09c5c084a129b27f79d2dd6ecc5475ec9d404fe
insignificantpylon <znastidobrostoje@gmail.com>
2025-03-01 20:41:53 +0100

    Add files via upload

--- Browser.py
+++ Browser.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout, QWidget,
-    QHBoxLayout, QPushButton, QLineEdit
+    QHBoxLayout, QPushButton, QLineEdit, QFileDialog, QMessageBox, QLabel, QProgressBar
 )
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtCore import Qt, QUrl, QPoint
+from PyQt6.QtWebEngineCore import QWebEngineProfile, QWebEngineDownloadRequest
+from PyQt6.QtCore import Qt, QUrl, QPoint, QTimer
 
 class Browser(QMainWindow):
     def __init__(self):
@@ -22,8 +23,10 @@ class Browser(QMainWindow):
         self.browser = QWebEngineView(self.central_widget)
         self.browser.setUrl(QUrl("https://www.google.com"))
         self.browser.urlChanged.connect(self.update_url_bar)
+        self.browser.page().profile().downloadRequested.connect(self.handle_download)
         
         self.create_navigation_bar()
+        self.create_download_status()
         
         self.layout.addWidget(self.browser)
         
@@ -33,16 +36,16 @@ class Browser(QMainWindow):
 
     def create_navigation_bar(self):
         nav_bar = QWidget(self.central_widget)
-        nav_bar.setFixedHeight(40)  # Reduce height to match button height
+        nav_bar.setFixedHeight(40)
         nav_layout = QHBoxLayout(nav_bar)
         nav_layout.setContentsMargins(5, 0, 5, 0)
         nav_layout.setSpacing(5)
         
-        self.back_button = QPushButton("◀", self)
+        self.back_button = QPushButton("\u25C0", self)
         self.back_button.setFixedSize(30, 30)
         self.back_button.clicked.connect(self.browser.back)
         
-        self.reload_button = QPushButton("⟳", self)
+        self.reload_button = QPushButton("\u27F3", self)
         self.reload_button.setFixedSize(30, 30)
         self.reload_button.clicked.connect(self.browser.reload)
         
@@ -53,16 +56,57 @@ class Browser(QMainWindow):
         nav_layout.addWidget(self.reload_button)
         nav_layout.addWidget(self.url_bar, 1)
         
-        self.layout.insertWidget(0, nav_bar)  # Ensure it's at the very top
+        self.layout.insertWidget(0, nav_bar)
+    
+    def create_download_status(self):
+        self.download_label = QLabel("", self)
+        self.download_label.setVisible(False)
+        
+        self.download_progress = QProgressBar(self)
+        self.download_progress.setVisible(False)
+        
+        self.layout.addWidget(self.download_label)
+        self.layout.addWidget(self.download_progress)
     
     def load_url(self):
         url = self.url_bar.text()
         if not url.startswith("http"):
-            url = "https://" + url
+            if "." in url:
+                url = "https://" + url
+            else:
+                url = f"https://www.google.com/search?q={url}"
         self.browser.setUrl(QUrl(url))
     
     def update_url_bar(self, qurl):
         self.url_bar.setText(qurl.toString())
+    
+    def handle_download(self, download: QWebEngineDownloadRequest):
+        save_path, _ = QFileDialog.getSaveFileName(self, "Save File", download.suggestedFileName())
+        if save_path:
+            download.setDownloadDirectory(save_path.rsplit("/", 1)[0])
+            download.setDownloadFileName(save_path.rsplit("/", 1)[-1])
+            download.accept()
+            
+            self.download_label.setText(f"Downloading: {download.suggestedFileName()}")
+            self.download_label.setVisible(True)
+            self.download_progress.setVisible(True)
+            self.download_progress.setValue(0)
+            
+            self.monitor_download(download)
+    
+    def monitor_download(self, download):
+        self.download_timer = QTimer(self)
+        self.download_timer.timeout.connect(lambda: self.update_download_progress(download))
+        self.download_timer.start(500)
+    
+    def update_download_progress(self, download):
+        if download.isFinished():
+            self.download_timer.stop()
+            self.download_label.setText("Download Complete")
+            self.download_progress.setValue(100)
+        elif download.totalBytes() > 0:
+            progress = int((download.receivedBytes() / download.totalBytes()) * 100)
+            self.download_progress.setValue(progress)
     
     def resizeEvent(self, event):
         self.overlay.setGeometry(0, 0, self.width(), 40)
@@ -71,13 +115,13 @@ class Browser(QMainWindow):
 class DragOverlay(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, False)
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, True)
         self._dragging = False
         self._drag_position = QPoint()
-        self.setFixedHeight(40)  # Match the height of navigation bar
+        self.setFixedHeight(40)
 
     def mousePressEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
+        if event.button() == Qt.MouseButton.LeftButton and event.position().y() < 40:
             self._dragging = True
             self._drag_position = event.globalPosition().toPoint() - self.window().pos()
             event.accept()
